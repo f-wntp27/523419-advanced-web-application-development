4e02021bd2dd31f1caf9e1c42f51bb702b299cd7
f-wntp27 <wntp27th@gmail.com>
2022-08-30 13:57:50 +0700

    lab4 - first commit

---
+++ lab4/lab4-angular-app/src/app/components/signup/signup.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    title: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    sex: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    stdid: new FormControl('', [Validators.required, Validators.pattern('B[0-9]{7}')]),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+    aliases: new FormArray([
+      new FormControl('')
+    ])
+  });
+
+  get email() {
+    return this.profileForm.get('email');
+  }
+
+  get stdid() {
+    return this.profileForm.get('stdid');
+  }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAliases() {
+    this.aliases.push(new FormControl(''));
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
